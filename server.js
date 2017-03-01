c78878ae088e0a26f892330062dc5ab446d66ce1
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-03-01 20:40:15 +0000

    Styled index file

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
-var multer  = require('multer');
- 
 var app = express();
+var multer  = require('multer');
 
 app.post('/get-file-size', multer({dest: './uploads/'}).single('file'), function(req, res){
-	console.log(req.file);
+	console.log('Checked size of ' + req.file.originalname);
 	res.send({size: req.file.size});
 });
 
-app.use(express.static(__dirname + '/public'));
+app.get('/:anything', function(req, res) {
+    res.redirect('/');
+});
+
+app.use(express.static(__dirname + '/client'));
 
 app.listen(process.env.PORT || 8080, function() {
     console.log('Listening on port 8080');
